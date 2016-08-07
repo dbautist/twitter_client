abd2e9d4335dd3824f6ff820feef2409f150ac04
Dianne Bautista <dianne.bautista@tapjoy.com>
2016-08-06 18:11:16 -0700

    style tweet details

--- app/src/main/java/com/codepath/apps/twitterclient/activities/TweetDetailActivity.java
+++ app/src/main/java/com/codepath/apps/twitterclient/activities/TweetDetailActivity.java
@@ -1,18 +1,22 @@
 package com.codepath.apps.twitterclient.activities;
 
+import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
+import android.text.Layout;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -44,15 +48,22 @@ public class TweetDetailActivity extends AppCompatActivity {
   ImageView ivProfilePhoto;
   @BindView(R.id.etReplyText)
   EditText etReplyText;
-  @BindView(R.id.btReply)
-  Button btReply;
+  @BindView(R.id.btReplyTweet)
+  Button btReplyTweet;
   @BindView(R.id.tvTime)
   TextView tvTime;
   @BindView(R.id.tvDate)
   TextView tvDate;
   @BindView(R.id.tvCharsLeft)
   TextView tvCharsLeft;
+  @BindView(R.id.buttonLayout)
+  RelativeLayout buttonLayout;
+  @BindView(R.id.ivMedia)
+  ImageView ivMedia;
+  @BindView(R.id.vReplyTweet)
+  View vReplyTweet;
 
+  private ActivityTweetDetailBinding mBinding;
   private TwitterClient mClient;
   private Tweet mTweet;
   private int maxCharLength;
@@ -60,7 +71,7 @@ public class TweetDetailActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    ActivityTweetDetailBinding binding = DataBindingUtil.setContentView(
+    mBinding = DataBindingUtil.setContentView(
         this, R.layout.activity_tweet_detail);
     ButterKnife.bind(this);
 
@@ -74,7 +85,7 @@ public class TweetDetailActivity extends AppCompatActivity {
 
     mTweet = Parcels.unwrap(getIntent().getParcelableExtra(AppConstants.TWEET_EXTRA));
     if (mTweet != null) {
-      binding.setTweet(mTweet);
+      mBinding.setTweet(mTweet);
       initTweetDetails();
     } else {
       // TODO: handle error
@@ -82,15 +93,32 @@ public class TweetDetailActivity extends AppCompatActivity {
   }
 
   private void initTweetDetails() {
+    Log.d(TAG, "---- initTweetDetails");
+
+    vReplyTweet.setVisibility(View.GONE);
     tvDate.setText(DateUtil.getFormattedDate(this, mTweet.createdAt));
     tvTime.setText(DateUtil.getFormattedTime(this, mTweet.createdAt));
+
+    if (mTweet.media != null) {
+      ivMedia.setVisibility(View.VISIBLE);
+      Glide.with(this).load(mTweet.media.mediaUrl) // .placeholder(R.drawable.loading_placeholder)
+          .centerCrop()
+          .into(ivMedia);
+    }
 
     etReplyText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
       @Override
       public void onFocusChange(View v, boolean hasFocus) {
-        if (hasFocus && TextUtils.isEmpty(etReplyText.getText())) {
-          etReplyText.setText(mTweet.user.screenName + " ");
-          etReplyText.setSelection(etReplyText.getText().length());
+        if (hasFocus) {
+          etReplyText.setLines(3);
+          if (TextUtils.isEmpty(etReplyText.getText())) {
+            etReplyText.setText(mTweet.user.screenName + " ");
+            etReplyText.setSelection(etReplyText.getText().length());
+          }
+
+          if (buttonLayout.getVisibility() == View.GONE) {
+            buttonLayout.setVisibility(View.VISIBLE);
+          }
         }
       }
     });
@@ -118,7 +146,7 @@ public class TweetDetailActivity extends AppCompatActivity {
         }
 
         tvCharsLeft.setTextColor(ContextCompat.getColor(TweetDetailActivity.this, charTextColor));
-        btReply.setEnabled(isEnabled);
+        btReplyTweet.setEnabled(isEnabled);
       }
     });
 
@@ -127,17 +155,19 @@ public class TweetDetailActivity extends AppCompatActivity {
         .into(ivProfilePhoto);
   }
 
-  @OnClick(R.id.btReply)
+  @OnClick(R.id.btReplyTweet)
   public void replyTweet() {
+    Log.d(TAG, "---- replyTweet");
+
     mClient.postStatus(etReplyText.getText().toString(), mTweet.id, new JsonHttpResponseHandler() {
       @Override
       public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
         Log.d(TAG, "----- tweet reply successful: " + response.toString());
         JSONDeserializer<Tweet> deserializer = new JSONDeserializer<>(Tweet.class);
-        Tweet statusTweet = deserializer.configureJSONObject(response);
-        if (statusTweet != null) {
+        Tweet replyTweet = deserializer.configureJSONObject(response);
+        if (replyTweet != null) {
           Log.d(TAG, "------ reply success");
-          handleSuccess();
+          handleSuccess(replyTweet);
         } else {
           handleError("Error from deserializing JSON response");
         }
@@ -160,13 +190,38 @@ public class TweetDetailActivity extends AppCompatActivity {
     });
   }
 
-  private void handleSuccess() {
-    // 1. Clear the edittext
+  private void handleSuccess(Tweet replyTweet) {
+    // hide keyboard
+    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+    imm.hideSoftInputFromWindow(etReplyText.getWindowToken(), 0);
     etReplyText.setText("");
+    etReplyText.setLines(1);
+    etReplyText.setSelection(0);
+    etReplyText.clearFocus();
+
+    if (buttonLayout.getVisibility() == View.VISIBLE) {
+      buttonLayout.setVisibility(View.GONE);
+    }
+
+    ReplyLayout replyLayout = new ReplyLayout();
+    ButterKnife.bind(replyLayout, vReplyTweet);
+    vReplyTweet.setVisibility(View.VISIBLE);
+    mBinding.vReplyTweet.setTweet(replyTweet);
+    if (replyTweet.user != null) {
+      Glide.with(this).load(replyTweet.user.profileImageUrl) // .placeholder(R.drawable.loading_placeholder)
+          .fitCenter().centerCrop()
+          .into(replyLayout.ivProfilePhoto);
+    }
   }
 
   private void handleError(String errorMessage) {
     ErrorHandler.logAppError(errorMessage);
     ErrorHandler.displayError(this, AppConstants.DEFAULT_ERROR_MESSAGE);
   }
+
+  public static class ReplyLayout {
+    @BindView(R.id.ivProfilePhoto)
+    ImageView ivProfilePhoto;
+  }
 }
+

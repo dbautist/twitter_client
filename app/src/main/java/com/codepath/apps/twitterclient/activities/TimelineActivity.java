9f52d633ba0e6e59c5359e384adf38c4539a2058
Dianne Bautista <dianne.bautista@tapjoy.com>
2016-08-11 18:11:16 -0700

    allow user to navigate to their profile

--- app/src/main/java/com/codepath/apps/twitterclient/activities/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/twitterclient/activities/TimelineActivity.java
@@ -1,11 +1,17 @@
 package com.codepath.apps.twitterclient.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.astuetz.PagerSlidingTabStrip;
+import com.bumptech.glide.Glide;
 import com.codepath.apps.twitterclient.R;
 import com.codepath.apps.twitterclient.TwitterApplication;
 import com.codepath.apps.twitterclient.adapters.TimelinePagerAdapter;
@@ -13,11 +19,14 @@ import com.codepath.apps.twitterclient.models.TweetManager;
 import com.codepath.apps.twitterclient.models.User;
 import com.codepath.apps.twitterclient.network.JSONDeserializer;
 import com.codepath.apps.twitterclient.network.TwitterClient;
+import com.codepath.apps.twitterclient.util.AppConstants;
 import com.codepath.apps.twitterclient.util.ErrorHandler;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.parceler.Parcels;
+import org.w3c.dom.Text;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,6 +35,8 @@ import cz.msebera.android.httpclient.Header;
 public class TimelineActivity extends AppCompatActivity {
   private static final String TAG = TimelineActivity.class.getSimpleName();
 
+  @BindView(R.id.toolbar)
+  Toolbar toolbar;
   @BindView(R.id.tabs)
   PagerSlidingTabStrip tabs;
   @BindView(R.id.viewpager)
@@ -40,15 +51,10 @@ public class TimelineActivity extends AppCompatActivity {
     setContentView(R.layout.activity_timeline);
     ButterKnife.bind(this);
 
-    if (getSupportActionBar() != null) {
-      getSupportActionBar().setDisplayShowHomeEnabled(true);
-      getSupportActionBar().setDisplayUseLogoEnabled(true);
-      getSupportActionBar().setLogo(R.drawable.twitter_logo_white_on_blue);
-    }
+    setSupportActionBar(toolbar);
 
     mClient = TwitterApplication.getRestClient();
     initViewPager();
-
     getCurrentUser();
   }
 
@@ -61,7 +67,22 @@ public class TimelineActivity extends AppCompatActivity {
 
   private void setUserInfo() {
     Log.d(TAG, "User: " + mCurrentUser.toString());
-    getSupportActionBar().setTitle(mCurrentUser.screenName);
+    ImageView toolbarImage = (ImageView) toolbar.findViewById(R.id.toolbarImage);
+    toolbarImage.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent intent = new Intent(TimelineActivity.this, ProfileActivity.class);
+        intent.putExtra(AppConstants.USER_EXTRA, Parcels.wrap(mCurrentUser));
+        startActivity(intent);
+      }
+    });
+
+//    Glide.with(this).load(mCurrentUser.profileImageUrl)
+//        .fitCenter().centerCrop()
+//        .into(toolbarImage);
+
+    TextView toolbarTitle = (TextView) toolbar.findViewById(R.id.toolbarTitle);
+    toolbarTitle.setText(mCurrentUser.screenName);
   }
 
   private void getCurrentUser() {
